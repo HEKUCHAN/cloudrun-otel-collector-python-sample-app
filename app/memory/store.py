71025a543a6f262a8083e0c907437fe6706752e8
Heitor Hirose <heitorhirose@gmail.com>
2025-12-08 11:42:39 +0900

    run linter

--- app/memory/store.py
+++ app/memory/store.py
@@ -1,10 +1,13 @@
 todos: list[dict] = []
 
+
 def add(todo):
     todos.append(todo)
 
+
 def find(id):
     return next((t for t in todos if t["id"] == id), None)
+
 
 def delete(id):
     global todos
--- app/router/todo.py
+++ app/router/todo.py
@@ -5,19 +5,23 @@ from app.models.todo import Todo
 
 router = APIRouter()
 
+
 @router.get("/")
 def list_todo():
     return todos
+
 
 @router.post("/")
 def create_todo(todo: Todo):
     add(todo.dict())
     return todo
 
+
 @router.delete("/{id}")
 def delete_todo(id: int):
     delete(id)
     return {"deleted": id}
+
 
 @router.patch("/{id}")
 def update_todo(id: int, patch: dict):
